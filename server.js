96c8a56d7fae75b7174630e581a5bf3ca2389b83
Dave Hoover <dave.hoover@gmail.com>
2010-04-30 08:57:04 -0500

    Hooking it up to MongoHQ

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 var http = require('http'),
-    port = process.env.PORT || 8001
+    port = process.env.PORT || 8001,
+    mongo = require('./vendor/node-mongodb-native/lib/mongodb')
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Hello Squirrels!\n')
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.write("<h1>Hello..." + Math.random()  +"</h1>")
+
+  var db = new mongo.Db('squirrel-node', new mongo.Server("flame.local.mongohq.com", 27024, {}))
+  db.open(function(err, db) {
+    db.authenticate("squirrel", "password", function(err, replies) {
+      if (err) {
+        res.write("<h3>UNAUTH!!! " + err + "</h3>")
+      }
+      db.collection('squirrels', function(err, collection) {
+        collection.find({}, {'sort':[['name', 1]]}, function(err, cursor) {
+          cursor.each(function(err, squirrel) {
+            if (squirrel) {
+              res.write(squirrel.name + "<br />")            
+            } else {
+              res.end("<h2>Thanks!</h2>")
+            }
+          })
+        })
+      })
+    })
+  })
+
 }).listen(parseInt(port))
